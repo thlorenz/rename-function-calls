e710daeec853e86eccd959da1b0888de52af86c6
Thorsten Lorenz <thlorenz@gmx.de>
2014-02-26 11:42:54 -0500

    renaming all calls successfully with patched detective

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 'use strict';
 
-var esprima = require('esprima-six')
-  , select = require('JSONSelect')
-  , esprimaOpts = { 
-      range: true
-    , parse: { tolerant: true } 
-    };
+var detective = require('detective')
+  , esprimaOpts = { tolerant: true, range: true };
 
 function rangeComparator(a, b) {
   return a.from > b.from ? 1 : -1;
 }
 
-function getReplacements(fromName, toName) {
+function getReplacements(fromName, toName, src) {
   var regex = new RegExp('^' + fromName);
 
-  var ast = esprima.parse(src, esprimaOpts);
-  var calls = select
-    .match('.type:val("ExpressionStatement") ~ .expression', ast)
-
-    // todo: how do I express this filter in JSONSelect and still pull out the whole ExpressionStatement?
-    .filter(function (n) { 
-      return n.type === 'CallExpression' 
-          && n.callee && n.callee.name === fromName 
-    })
-
-  return calls
-    .map(function (n) { 
-      var code = src.slice(n.range[0], n.range[1]).replace(regex, toName);
-      return { from: n.range[0], to: n.range[1], code: code };
-    });
+  var res = detective.find(src, { word: fromName, nodes: true, parse: esprimaOpts });
+  return res.nodes.map(function (n) {
+    var c = n.callee;
+      var code = src.slice(c.range[0], c.range[1]).replace(regex, toName);
+      return { from: c.range[0], to: c.range[1], code: code };
+  });
 }
 
 var go = module.exports = 
@@ -46,7 +33,7 @@ var go = module.exports =
  * @param {string} origSrc the original source
  * @param {string} fromName name under which function is currently called
  * @param {string} toName name to which the function calls should be renamed
- * @return {string} source with function calls renamed exposed
+ * @return {string} source with function calls renamed
  */
 function rename(fromName, toName, origSrc) {
   var src = origSrc;
@@ -57,7 +44,7 @@ function rename(fromName, toName, origSrc) {
   if (!regex.test(src)) return src
 
   var offset = 0;
-  return getReplacements(toName, toName)
+  return getReplacements(fromName, toName, src)
     .sort(rangeComparator)
     .reduce(function(acc, replacement) {
       var from = replacement.from + offset
@@ -84,36 +71,7 @@ if (!module.parent && typeof window === 'undefined') {
   ].join('\n');
 
   var fromName = 'x';
-  var toName = 'y';
+  var toName = 'replacedX';
 
-  var regex = new RegExp('^' + fromName);
-
-  var ast = esprima.parse(src, esprimaOpts);
-  var calls = select
-    .match('.type:val("CallExpression") ~ :root', ast)
-    // todo: how do I express this filter in JSONSelect?
-    .filter(function (n) { return n.name === fromName })
-
-
+  go(fromName, toName, src);
 }
-  /*var replacements = calls
-    .map(function (n) { 
-      var code = src.slice(n.range[0], n.range[1]).replace(regex, 'y');
-      return { from: n.range[0], to: n.range[1], code: code };
-    });
-
-  var offset = 0;
-  var s = replacements
-    .sort(rangeComparator)
-    .reduce(function(acc, replacement) {
-      var from = replacement.from + offset
-        , to   = replacement.to + offset
-        , code = replacement.code;
-
-      // all ranges will be invalidated since we are changing the code
-      // therefore keep track of the offset to adjust them in case we replace multiple requires
-      var diff = code.length - (to - from);
-      offset += diff;
-      return acc.slice(0, from) + code + acc.slice(to);
-    }, src);
-*/
